ee0100679404a5d44905c8cf783b7ba1694df49a
Justin Patrin <papercrane@reversefold.com>
2015-02-26 15:21:24 -0800

    PE-270 extend the copyright

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'krux-stdlib'
-copyright = u'2015, Krux Digital, Inc.'
+copyright = u'2013-2015, Krux Digital, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
