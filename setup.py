ba5e7348b75a169404deeeb6c38695ec9f874910
Paul Lathrop <paul@tertiusfamily.net>
2015-04-13 16:51:15 -0700

    Remove tornado module.
    This should have been a separate package from the very beginning. I have
    created krux/python-krux-tornado to replace the tornado module(s) from
    stdlib.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 
 # We use the version to construct the DOWNLOAD_URL.
-VERSION = '0.9.0'
+VERSION = '1.0.0'
 
 # URL to the repository on Github.
 REPO_URL = 'https://github.com/krux/python-krux-stdlib'
