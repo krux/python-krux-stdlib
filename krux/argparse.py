0f4bcfa7e0e938ac929dd336ed4ee1477f077cc0
Paul Lathrop <plathrop@salesforce.com>
2017-04-17 16:28:41 -0400

    Update docstring, comment.

--- krux/argparse.py
+++ krux/argparse.py
@@ -33,8 +33,20 @@ class AppendOverDefault(Action):
     >>> options = parser.parse_args(['--ips', '1.2.3.4', '--ips', '5.6.7.8'])
     >>> print options.ips
     ['1.2.3.4', '5.6.7.8']
+
+    Note that just like `action='append'` this Action does not
+    de-duplicate options:
+
+    >>> options = parser.parse_args(['--ips', '1.2.3.4', '--ips', '1.2.3.4'])
+    >>> print options.ips
+    ['1.2.3.4', '1.2.3.4']
     """
     def __call__(self, parser, namespace, values, option_string=None):
+        # This will be called once per incidence of the argument being
+        # specified on the command line (and zero times if the option
+        # is NOT specified). So, for the examples in the class
+        # docstring, this code will be called zero, one, and two
+        # times, respectively.
         if getattr(namespace, self.dest) == self.default:
             setattr(namespace, self.dest, [values])
         else:
