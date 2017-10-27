3e1e337de714ab6ddaf6bda1f6b4a10685a307a5
Peter Han <phan@salesforce.com>
2017-10-26 20:25:53 -0700

    Environment variable fall back for CLI arguments

--- krux/parser.py
+++ krux/parser.py
@@ -7,6 +7,7 @@
 # Standard libraries
 #
 from __future__ import absolute_import
+from os import environ
 
 #
 # Third party libraries
@@ -37,7 +38,7 @@ def get_parser(
     # GOTCHA: KruxParser takes in a logger and a stats object. However, "stats" parameter is already defined
     #         as a boolean flag. Do not change the name of that parameter and break backward compatibility.
     # TODO: Update and clean up the parameter names so we can pass logger and stats into KruxParser.
-    parser = KruxParser(ArgumentParser(*args, **kwargs))
+    parser = KruxParser(wrapped=ArgumentParser(*args, **kwargs))
 
     # standard logging arguments
     if logging:
@@ -63,19 +64,17 @@ def add_logging_args(parser, stdout_default=True):
 
     :argument parser: parser instance to which the arguments will be added
     """
-    group = get_group(parser, 'logging')
+    group = get_group(parser=parser, group_name='logging', prefix=False)
 
     group.add_argument(
         '--log-level',
         default=DEFAULT_LOG_LEVEL,
-        #env_var='LOG_LEVEL',
         choices=LEVELS.keys(),
         help='Verbosity of logging. (default: %(default)s)'
     )
     group.add_argument(
         '--log-file',
         default=None,
-        #env_var='LOG_FILE',
         help='Full-qualified path to the log file '
         '(default: %(default)s).'
     )
@@ -91,7 +90,6 @@ def add_logging_args(parser, stdout_default=True):
     group.add_argument(
         '--syslog-facility',
         default=DEFAULT_LOG_FACILITY,
-        #env_var='SYSLOG_FACILITY',
         help='syslog facility to use '
         '(default: %(default)s).'
     )
@@ -134,7 +132,7 @@ def add_stats_args(parser):
 
     :argument parser: parser instance to which the arguments will be added
     """
-    group = get_group(parser, 'stats')
+    group = get_group(parser=parser, group_name='stats', prefix=False)
 
     group.add_argument(
         '--stats',
@@ -162,7 +160,7 @@ def add_stats_args(parser):
 
 
 def add_lockfile_args(parser):
-    group = get_group(parser, 'lockfile')
+    group = get_group(parser=parser, group_name='lockfile', prefix=False)
 
     group.add_argument(
         '--lock-dir',
@@ -173,7 +171,7 @@ def add_lockfile_args(parser):
     return parser
 
 
-def get_group(parser, group_name):
+def get_group(parser, group_name, prefix=None):
     """
     Return an argument group based on the group name.
 
@@ -193,18 +191,67 @@ def get_group(parser, group_name):
     if group_name in groups:
         group = parser._action_groups[groups.index(group_name)]
     else:
-        group = parser.add_argument_group(group_name)
+        group = parser.add_argument_group(title=group_name, prefix=prefix)
 
     return group
 
 
 class KruxParser(Wrapper):
-    def add_argument_group(self, *args, **kwargs):
-        group = KruxGroup(wrapped=_ArgumentGroup(self, *args, **kwargs), logger=self._logger, stats=self._stats)
+    def add_argument_group(self, prefix, *args, **kwargs):
+        group = KruxGroup(
+            wrapped=_ArgumentGroup(self, *args, **kwargs), prefix=prefix, logger=self._logger, stats=self._stats,
+        )
         self._wrapped._action_groups.append(group)
         return group
 
 
 class KruxGroup(Wrapper):
+    def __init__(self, prefix=None, *args, **kwargs):
+        """
+        Creates a wrapper around argparse._ArgumentGroup that handles some help doc automation as well as environment
+        variable fall back
+
+        :param prefix: Prefix to use for the environment variables. If set to None, uses the title
+                       of the _ArgumentGroup that this is wrapping. If set to False, does not add any prefix
+        :type prefix: str | bool
+        :param args: Ordered arguments passed directly to krux.wrapper.Wrapper.__init__()
+        :type args: list
+        :param kwargs: Keyword arguments passed directly to krux.wrapper.Wrapper.__init__()
+        :type kwargs: dict
+        """
+        # Call to the superclass to bootstrap.
+        super(KruxGroup, self).__init__(*args, **kwargs)
+
+        if prefix is None:
+            self._env_prefix = self._wrapped.title + '_'
+        elif prefix is False:
+            self._env_prefix = ''
+        else:
+            self._env_prefix = str(prefix) + '_'
+
     def add_argument(self, *args, **kwargs):
+        use_env_var = kwargs.pop('use_env_var', True)
+
+        # There must be an argument defined and it must be prefixed. (i.e. This does not handle positional arguments.)
+        if use_env_var and len(args) > 0 and args[0][0] in self.prefix_chars:
+            # Find the first long-named option
+            first_long = next((arg_name for arg_name in args if arg_name[1] in self.prefix_chars), None)
+
+            # There must be a long name or environment variable support is explicitly turned off.
+            if first_long is None:
+                raise Exception(
+                    'You must either disable the environment variable fall back or provide a long name for the option'
+                )
+
+            # Determine the key of the environment variable for this argument
+            key = first_long.lstrip(self.prefix_chars)
+            if not key:
+                raise Exception(
+                    'You must provide a valid name for the option'
+                )
+            key = (self._env_prefix + key.replace('-', '_')).upper()
+
+            # Override the default value to use the environment variable
+            kwargs['default'] = environ.get(key=key, failobj=kwargs.get('default', None))
+
         return self._wrapped.add_argument(*args, **kwargs)
