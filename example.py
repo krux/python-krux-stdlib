0d429ecdd17c7a9bb27d4c813ffbbcc60a946141
dllopis-sfdc <496931+dllopis-sfdc@users.noreply.github.com>
2017-11-13 21:10:03 -0800

    Improvements to example.py

--- example.py
+++ example.py
@@ -8,6 +8,9 @@ class ExampleLoggingApp(krux.cli.Application):
     This class gets a few things for free; not least of which:
       * a command-line option parser
       * a logger
+      * a stats dispatcher
+      * a context manager, which cleans up after us,
+        including deleting the lock file, if one was created.
     """
     def __init__(
         self,
@@ -30,10 +33,46 @@ class ExampleLoggingApp(krux.cli.Application):
             parser_args=parser_args,
         )
 
+        # If you have command line arguments, this is when you want
+        # to get retrieve & store them. super().__init__() will have stored them
+        # self.args for us. The only place we want to read from self.args
+        # is here in our __init__().
+        self.bit_bucket_kg = self.args.bit_bucket_kg
+        self.has_headlight_fluid = self.args.has_headlight_fluid
+
+        # Do any other initialization tasks here
+        pass
+
+    # If your app wants to add its own command line arguments, do it here.
+    # If not, then you don't need to create this method.
+    def add_cli_arguments(self, parser):
+        super(ExampleLoggingApp, self).add_cli_arguments(parser)
+
+        # See the docstring & comments in get_group()
+        group = krux.cli.get_group(self.parser, self.name)
+        # Build arguments as you would with ArgumentParser
+        # https://docs.python.org/2.7/library/argparse.html
+        group.add_argument(
+            '--bit-bucket-kg',
+            default=5,
+            help='Bit bucket weight in kg.',
+        )
+        group.add_argument(
+            '-f', '--has-headlight-fluid',
+            default=False,
+            action='store_true',
+            help='Contains sufficient headlight fluid.',
+        )
+
     def run(self):
         """
         Trivial example of calling at least one thing.
         """
+        print('The bit bucket weighs {} kg.'.format(self.bit_bucket_kg))
+        if self.has_headlight_fluid:
+            print('We have enough headlight fluid.')
+        else:
+            print('We could use more headlight fluid.')
         # the default log level is "warn" so you won't see these uness
         # you pass in --log-level from the command line
         self.logger.debug("this is a debug level message")
